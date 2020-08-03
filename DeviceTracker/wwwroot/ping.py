e75f77c9e4e5626c3052a7f27e0e9476c1aa49e1
Niek <git@niekvantoepassing.nl>
2020-08-03 22:11:23 +0200

    add a ping shell script

--- DeviceTracker/wwwroot/ping.py
+++
@@ -1,4 +0,0 @@
-import requests
-import os
-
-response = requests.get(os.environ['PING_URL'] + "/?device=" + os.environ['DEVICE'])
---
+++ DeviceTracker/wwwroot/ping.sh
@@ -0,0 +1,16 @@
+# IP opslaan in een txt file. apparaat.txt bestaat al met de apparaatnaam
+echo "IP: " >~/extra.txt
+/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' >> ~/extra.txt
+    
+# Uptime opslaan in een txt
+echo "Uptime: " >>~/extra.txt
+/usr/bin/uptime | grep -ohe 'up .*' | sed 's/,//g' | awk '{ print $2 }' >> ~/extra.txt
+
+# Beschikbare schijfruimte naar txt
+echo "Free diskspace: " >> ~/extra.txt
+/bin/df -h | tr -s ' ' $'\t' | grep /dev/root | cut -f4 >> ~/extra.txt
+    
+# Ping versturen naar de server
+curl -d "device=$(cat ~/apparaat.txt)&info=$(cat ~/extra.txt)" -X POST $DEVICE_TRACKER_URL/ping/ping
+
+wget $DEVICE_TRACKER_URL/ping.sh -O ~/ping.sh
