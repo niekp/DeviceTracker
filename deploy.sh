5452cff5d7cceb4dba001398c4952ca5dc1fc326
Niek <git@niekvantoepassing.nl>
2020-08-03 22:10:37 +0200

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-ssh -t ovh "sudo service device stop && sudo chown -R niek /opt/Device"
-rsync -avz  DeviceTracker/bin/Release/netcoreapp3.1/publish/* ovh:/opt/Device/
-ssh -t ovh "sudo chown -R www-data /opt/Device && sudo service device start"
+ssh -t ovh "sudo service devicetracker stop && sudo chown -R niek /opt/DeviceTracker"
+rsync -avz  DeviceTracker/bin/Release/netcoreapp3.1/publish/* ovh:/opt/DeviceTracker/
+ssh -t ovh "sudo chown -R www-data /opt/DeviceTracker && sudo service devicetracker start"
