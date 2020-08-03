bab493b83eddf8e0278ec0700ab148db1b3dd4fc
Niek <git@niekvantoepassing.nl>
2020-08-03 16:46:46 +0200

    Setup ping controller to recieve pings

---
+++ DeviceTracker/Controllers/PingController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DeviceTracker.Repositories;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DeviceTracker.Controllers
+{
+    public class PingController : Controller
+    {
+        private readonly IPingRepository pingRepository;
+        private readonly IDeviceRepository deviceRepository;
+
+        public PingController(IPingRepository pingRepository, IDeviceRepository deviceRepository)
+        {
+            this.pingRepository = pingRepository;
+            this.deviceRepository = deviceRepository;
+        }
+
+        public async Task<IActionResult> Ping(string device, string info = "")
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(info))
+                {
+                    await deviceRepository.SetInfo(device, info);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            await pingRepository.Ping(device);
+
+            return Ok();
+        }
+    }
+}
